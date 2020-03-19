229fe96b572f6cb3079616e8f7eeab0b9e8e9101
mctony999 <37092347+mctony999@users.noreply.github.com>
2020-03-20 00:36:39 +0800

    1.0 finish
    Finish lantern and eatable lantern

--- src/main/java/com/Tonyloser/lampmod/blocks/BlockBase.java
+++ src/main/java/com/Tonyloser/lampmod/blocks/BlockBase.java
@@ -7,6 +7,7 @@ import com.Tonyloser.lampmod.util.IHasModel;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
@@ -29,4 +30,6 @@ public class BlockBase extends Block implements IHasModel
 	{
 		Main.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");
 	}
+	
+	
 }
--- src/main/java/com/Tonyloser/lampmod/blocks/GroundLamp.java
+++ src/main/java/com/Tonyloser/lampmod/blocks/GroundLamp.java
@@ -2,11 +2,17 @@ package com.Tonyloser.lampmod.blocks;
 
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 
 public class GroundLamp extends BlockBase
 {
-
-	public GroundLamp(String name, Material material) 
+	public static final AxisAlignedBB GROUND_LAMP_BLOCK_AABB = new AxisAlignedBB(0.0625 * 5, 0, 0.0625*5, 0.0625*11, 0.0625*10, 0.0625*11); //collision control, set to point in the space and the rectangle will be the collision space
+	
+	public GroundLamp(String name, Material material) //block property
 	{
 		super(name, material);
 		
@@ -19,5 +25,25 @@ public class GroundLamp extends BlockBase
 		//setBlockUnbreakable();
 	}
 	
+	@Override
+	public boolean isOpaqueCube(IBlockState stat) //make the floor can see
+	{
+		return false;
+	}
+	
+	public boolean isFullCube(IBlockState state) 
+	{
+		return false;
+	}
+	
+	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
+	{
+		return GROUND_LAMP_BLOCK_AABB;
+	}
+	
+	public BlockRenderLayer getBlockLayer() //Make it's texture partial transparent
+	{
+		return BlockRenderLayer.CUTOUT_MIPPED;
+	}
 	
 }
